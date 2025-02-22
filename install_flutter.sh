c54cbc49a047f79027e4b69032f4a5da83146b9c
Jose Selma Hoyo <jlselma25@gmail.com>
2025-02-22 19:32:59 +0100

    lasteset

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# Instalar dependencias necesarias
+sudo apt-get update
+sudo apt-get install -y ninja-build libgtk-3-dev
+
 # Descargar Flutter
 git clone https://github.com/flutter/flutter.git -b stable
 
-# Añadir Flutter al PATH
+# Añadir Flutter al PATH de manera temporal en el script
 export PATH="$PATH:$(pwd)/flutter/bin"
 
 # Verificar si Flutter está instalado
-flutter --version
+$(pwd)/flutter/bin/flutter --version
 
 # Asegurarse de que las dependencias de Flutter estén correctamente instaladas
-flutter doctor
+$(pwd)/flutter/bin/flutter doctor
+
+# Ahora construir el proyecto
+$(pwd)/flutter/bin/flutter build web
