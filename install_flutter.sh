fd389dacc2d3a2de8dff42437ab7665aa9991596
Jose Selma Hoyo <jlselma25@gmail.com>
2025-02-22 19:27:24 +0100

    ultimo

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
 # Descargar Flutter
 git clone https://github.com/flutter/flutter.git -b stable
 
 # Añadir Flutter al PATH
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
 
-# Verificar la instalación de Flutter
+# Verificar si Flutter está instalado
+flutter --version
+
+# Asegurarse de que las dependencias de Flutter estén correctamente instaladas
 flutter doctor
