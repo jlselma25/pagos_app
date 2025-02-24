9ecc047d231abdeeeb9697bb7a2b63413c789a8e
Jose Selma Hoyo <jlselma25@gmail.com>
2025-02-24 17:07:50 +0100

    cambio iconos

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4052706468"
+    serviceWorkerVersion: "3016330879"
   }
 });
