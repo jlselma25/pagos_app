a52a3761eda6547cbb39edf29629e7e861938785
Jose Selma Hoyo <jlselma25@gmail.com>
2025-03-07 21:22:56 +0100

    Add pdf

--- lib/helpers/numeros_decimales.dart
+++ lib/helpers/numeros_decimales.dart
@@ -3,17 +3,8 @@
 String numerosDecimalet(double importe){
 
   String newImporte ='';
-  final split = importe.toString().split('.');
-
-  if (split.length == 2){
-
-     if ( split[1].length == 1 ){
-        newImporte =  '${split[0]},${split[1].padRight(2,'0')}';
-    }  
-
-  }else{
-      newImporte =  '${split[0]},${split[1].padRight(2,'0')}';
-  }
+  final split = importe.toString().split('.'); 
+  newImporte =  '${split[0]},${split[1].padRight(2,'0')}';
 
   return  newImporte;
 
