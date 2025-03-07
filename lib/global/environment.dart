bea0534d16399b6c2c3c841df1752563f7aefab0
Jose Selma Hoyo <jlselma25@gmail.com>
2025-03-07 21:06:25 +0100

    Add pdf

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -10,6 +10,6 @@ class Environment {
   static String codigoRegistro = '220212';
   static  Color color = const Color(0xFF1F4079);
   static String proyecto = 'Pagos app';
-  static String version = 'v.3.2';
+  static String version = 'v.3.4';
 
 }
