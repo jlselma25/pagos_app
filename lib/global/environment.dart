10e3a61e15d63e81e079d28d448762bc8c38aa55
Jose Selma Hoyo <jlselma25@gmail.com>
2025-03-16 15:24:50 +0100

    totales en lista estadistica

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -12,7 +12,7 @@ class Environment {
   static String codigoRegistro = '220212';
   static  Color color = const Color(0xFF1F4079);
   static String proyecto = 'Pagos app';
-  static String version = 'v.4.6';
+  static String version = 'v.4.7';
   static Color colorTextoMark =  Colors.white;
   static Color colorText =  Colors.black;
 
